3987fe6bc5bd3eeaba54fb76489ad5e3edf14f22
christian-crisologo-lrn <christian.crisologo@learnosity.com>
2020-12-11 14:18:08 +1100

    add install

--- views/src/App.svelte
+++ views/src/App.svelte
@@ -175,17 +175,9 @@
 	<footer class="footer text-center">
 		<div class="container">
 			<div class="row">
-				<!-- Footer Location-->
-				<div class="col-lg-6 mb-5 mb-lg-0">
-					<h4 class="text-uppercase mb-4">Location</h4>
-					<p class="lead mb-0">
-						18/1 Margaret St
-						<br />
-						Sydney NSW 2000
-					</p>
-				</div>
+		
 				<!-- Footer Social Icons-->
-				<div class="col-lg-6 mb-5 mb-lg-0">
+				<div class="mb-5 mb-lg-0">
 					<h4 class="text-uppercase mb-4">Around the Web</h4>
 					<a class="btn btn-outline-light btn-social mx-1" href="#!"><i class="fab fa-fw fa-facebook-f"></i></a>
 					<a class="btn btn-outline-light btn-social mx-1" href="#!"><i class="fab fa-fw fa-twitter"></i></a>
